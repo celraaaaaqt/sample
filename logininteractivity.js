c682a1e65d6f0245c7d68ac28484f2e9efaf6a12
celraaaaaqt <arcelzamora068@gmail.com>
2025-07-08 17:04:09 +0800

    Add files via upload

---
+++ logininteractivity.js
@@ -0,0 +1,6 @@
+document.getElementById("home").addEventListener("click", function() {
+    window.location.href = "home.html";  // Redirects to home.html
+});
+document.getElementById("about").addEventListener("click", function() {
+    window.location.href = "about.html";  // Redirects to home.html
+});
